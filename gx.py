91097b3350bb03cd551335ffabb5c1614113ebd9
broholens <broholens@github.com>
2018-03-23 17:49:41 +0800

    add more teacher

--- gx.py
+++ gx.py
@@ -1,5 +1,3 @@
-import time
-import random
 from itertools import count
 from selenium import webdriver
 
@@ -7,13 +5,18 @@ options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 d = webdriver.Firefox(options=options)
 
-url = 'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=gaoxiang15@xdf.cn&schoolId=6&teacherCode=TC1540&source=woxueTeacher&sourceExt=TC1540&from=groupmessage&isappinstalled=0'
+urls = [
+    # 'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=gaoxiang15@xdf.cn&schoolId=6&teacherCode=TC1540&source=woxueTeacher&sourceExt=TC1540&from=groupmessage&isappinstalled=0',
+    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=weiyunpeng@xdf.cn&schoolId=6&teacherCode=TC1537&source=woxueTeacher&sourceExt=TC1537&from=groupmessage&isappinstalled=0'
+    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=hanyang9@xdf.cn&schoolId=6&teacherCode=TC1539&source=woxueTeacher&sourceExt=TC1539&from=groupmessage&isappinstalled=0',
+    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=liuchongyou@xdf.cn&schoolId=6&teacherCode=TC1546&source=woxueTeacher&sourceExt=TC1546&from=groupmessage&isappinstalled=0',
+    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=lichen44@xdf.cn&schoolId=6&teacherCode=TC1553&source=woxueTeacher&sourceExt=TC1553&from=groupmessage&isappinstalled=0'
+]
 
 for i in count():
-    print(i)
-    try:
-        d.get(url)
-        d.find_element_by_class_name('js-like').click()
-        time.sleep(random.randrange(1, 3))
-    except:
-        pass
+    for url in urls:
+        try:
+            d.get(url)
+            d.find_element_by_class_name('js-like').click()
+        except:
+            pass
