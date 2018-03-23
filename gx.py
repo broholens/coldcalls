993dc64a7838e90052a16cb1f50d0c319de49196
broholens <broholens@github.com>
2018-03-23 17:13:25 +0800

    --headless

--- gx.py
+++ gx.py
@@ -3,8 +3,9 @@ import random
 from itertools import count
 from selenium import webdriver
 
-
-d = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+d = webdriver.Firefox(options=options)
 
 url = 'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=gaoxiang15@xdf.cn&schoolId=6&teacherCode=TC1540&source=woxueTeacher&sourceExt=TC1540&from=groupmessage&isappinstalled=0'
 
