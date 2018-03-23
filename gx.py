9484de7a4820ad59f9660373a80b379c8c5788a0
broholens <broholens@github.com>
2018-03-23 17:08:38 +0800

    刷赞

---
+++ gx.py
@@ -0,0 +1,18 @@
+import time
+import random
+from itertools import count
+from selenium import webdriver
+
+
+d = webdriver.Firefox()
+
+url = 'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=gaoxiang15@xdf.cn&schoolId=6&teacherCode=TC1540&source=woxueTeacher&sourceExt=TC1540&from=groupmessage&isappinstalled=0'
+
+for i in count():
+    print(i)
+    try:
+        d.get(url)
+        d.find_element_by_class_name('js-like').click()
+        time.sleep(random.randrange(1, 3))
+    except:
+        pass
