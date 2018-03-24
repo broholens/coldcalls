6348ff3165b4eac758a2ed9b40b9b4ca7857d892
broholens <broholens@github.com>
2018-03-24 09:07:18 +0800

    use grequests

--- gx.py
+++ gx.py
@@ -1,25 +1,23 @@
-from itertools import count
-from selenium import webdriver
-
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
-d = webdriver.Firefox(options=options)
+import grequests
 
 urls = [
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=gaoxiang15@xdf.cn&schoolId=6&teacherCode=TC1540&source=woxueTeacher&sourceExt=TC1540&from=groupmessage&isappinstalled=0',
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=weiyunpeng@xdf.cn&schoolId=6&teacherCode=TC1537&source=woxueTeacher&sourceExt=TC1537&from=groupmessage&isappinstalled=0'
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=hanyang9@xdf.cn&schoolId=6&teacherCode=TC1539&source=woxueTeacher&sourceExt=TC1539&from=groupmessage&isappinstalled=0',
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=liuchongyou@xdf.cn&schoolId=6&teacherCode=TC1546&source=woxueTeacher&sourceExt=TC1546&from=groupmessage&isappinstalled=0',
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=lichen44@xdf.cn&schoolId=6&teacherCode=TC1553&source=woxueTeacher&sourceExt=TC1553&from=groupmessage&isappinstalled=0',
-    'https://ppss.xdf.cn/h5/teacher/index.html?userId=shangguanhe@xdf.cn&schoolId=6&teacherCode=TC1556&source=woxueTeacher&sourceExt=TC1556&from=singlemessage&isappinstalled=0',
-    'https://ppss.xdf.cn/h5/2-teacher/index.html?userId=wangqisong@xdf.cn&schoolId=6&teacherCode=TC1511&source=woxueTeacher&sourceExt=TC1511&from=singlemessage&isappinstalled=0'
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=gaoxiang15%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=weiyunpeng%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=hanyang%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=liuchongyou%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=lichen44%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=shangguanhe%40xdf.cn&protocol=https%3A',
+    'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=wangqisong%40xdf.cn&protocol=https%3A'
 ]
 
-for i in count():
-    print(i)
-    for url in urls:
-        try:
-            d.get(url)
-            d.find_element_by_class_name('js-like').click()
-        except:
-            pass
+def exception_handler(request, exception):
+    print('error')
+
+def g_request():
+    rs = (grequests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}) for url in urls)
+    tasks = grequests.imap(rs, exception_handler=exception_handler)
+    for task in tasks:
+        print(task.json())
+if __name__ =='__main__':
+    while True:
+        g_request() 
