3b07213f09d3c85b82258af9e2c267817ae9db7a
broholens <broholens@github.com>
2018-03-26 22:59:41 +0800

    use grequests

--- gx.py
+++ gx.py
@@ -10,14 +10,18 @@ urls = [
     'https://ppssp.xdf.cn/Mobile_Teacher_Home/like?u=wangqisong%40xdf.cn&protocol=https%3A'
 ]
 
+
 def exception_handler(request, exception):
     print('error')
 
+
 def g_request():
-    rs = (grequests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}) for url in urls)
+    rs = (grequests.get(url, timeout=5, headers={'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)'}) for url in urls)
     tasks = grequests.imap(rs, exception_handler=exception_handler)
     for task in tasks:
         print(task.json())
+
+        
 if __name__ =='__main__':
     while True:
         g_request() 
